c6cd3b0f4f020a8b74751b6bd42906a271d645be
Alex Chan <alch5804@colorado.edu>
2022-04-05 10:43:58 -0600

    update

---
+++ js/week12-demo.js
@@ -0,0 +1,33 @@
+// document.querySelector('button');
+// document.querySelector('.new-quote button');
+// const quoteButton document.querySelector('#js-new-quote');
+
+// look for a event to happen
+
+async function getQuote(){
+console.log("quote button was clicked");
+try{
+  const respone = await fetch(endpoint);
+  if(!response.ok){
+    throw Error (response.statusText)
+    }
+
+    const json= await response.json();
+    console.log(json);
+    displayQuote(json.fact);
+  } catch (err){
+      console.log(err);
+    alert('failed');
+
+  }
+}
+function displayQuote(quote){
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+}
+//https://random-quote-generator.herokuapp.com/api/quotes/random
+// https://catfact.ninja/fact
+const endpoint = 'https://catfact.ninja/fact';
+const quoteButton document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click',getQuote);
