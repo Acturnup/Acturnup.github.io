5345c27116a08139251855b96ff520cdc258356c
Alex Chan <alch5804@colorado.edu>
2022-04-07 12:21:02 -0600

    update

--- js/week12-demo.js
+++ js/week12-demo.js
@@ -9,14 +9,18 @@ console.log("quote button was clicked");
 try{
   const response = await fetch(endpoint);
   if(!response.ok){
+
     throw Error (response.statusText)
+         setData(data);
     }
 
     const json= await response.json();
     console.log(json);
-    displayQuote(json.fact);
+    displayQuote(json.content);
+    displayAuthor(json.author);
   } catch (err){
-      console.log(err);
+
+    console.log(err);
     alert('failed');
 
   }
@@ -25,10 +29,14 @@ function displayQuote(quote){
   const quoteText = document.querySelector('#js-quote-text');
   quoteText.textContent = quote;
 }
+function displayAuthor(quote){
+  const quoteText = document.querySelector('#js-quote-author');
+  quoteText.textContent = quote;
+}
 
 //https://random-quote-generator.herokuapp.com/api/quotes/random
 // https://catfact.ninja/fact
-const endpoint = 'https://catfact.ninja/fact';
+const endpoint = 'https://api.quotable.io/random ';
 const quoteButton = document.querySelector('#js-new-quote');
 quoteButton.addEventListener('click',getQuote);
 
