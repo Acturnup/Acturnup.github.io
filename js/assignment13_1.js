eda4cfa6882f8f0890bff62fa5622f3835e6eb17
Alex Chan <alch5804@colorado.edu>
2022-04-14 13:34:33 -0600

    update

--- js/assignment13_1.js
+++ js/assignment13_1.js
@@ -32,7 +32,7 @@ newStory = newStory.replace(':insertZ:',zItem);
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300/14) + ' stone ';
+    let weight = Math.round(300/14) + ' stone';
     let temperature =  Math.round((94-32) * 5/9) + ' centigrade';
     newStory = newStory.replace('300 pounds', weight);
     newStory = newStory.replace('94 fahrenheit', temperature);
